455bf2f244470881b601af80eb67fc389061fe60
coffeeboy123 <ih369369@naver.com>
2025-06-07 17:35:57 +0900

    dsfsd

--- model/lmbn_n.py
+++ model/lmbn_n.py
@@ -1,149 +1,179 @@
-import argparse
+import copy
+import torch
+from torch import nn
+from .osnet import osnet_x1_0, OSBlock
+from .attention import BatchDrop, BatchFeatureErase_Top, PAM_Module, CAM_Module, SE_Module, Dual_Module
+from .bnneck import BNNeck, BNNeck3
+from torch.nn import functional as F
 
-parser = argparse.ArgumentParser(description='MGN')
-
-parser.add_argument('--nThread', type=int, default=4,
-                    help='number of threads for data loading')
-parser.add_argument('--cpu', action='store_true',
-                    help='if raise, use cpu only')
-parser.add_argument('--nGPU', type=int, default=1, help='number of GPUs')
-
-parser.add_argument("--config", type=str, default="", help='config path')
+from torch.autograd import Variable
 
 
-parser.add_argument("--datadir", type=str,
-                    default="C:/Users/Howon_LEE/Desktop/Hard_Data/ReIDataset_Reg_A", help='dataset directory root')
-parser.add_argument('--data_train', type=str,
-                    default='REGDB', help='train dataset name')
-parser.add_argument('--data_test', type=str,
-                    default='REGDB', help='test dataset name')
-parser.add_argument('--cuhk03_labeled', action='store_true',
-                    help='if raise, use cuhk03-labeled dataset, otherwise cuhk03-detected dataset')
+class LMBN_n(nn.Module):
+    def __init__(self, args):
+        super(LMBN_n, self).__init__()
 
-parser.add_argument("--epochs", type=int, default=70,
-                    help='number of epochs to train')
-parser.add_argument('--test_every', type=int, default=1,
-                    help='do test per every N epochs')
-parser.add_argument("--batchid", type=int, default=6, help='the batch for id')
-parser.add_argument("--batchimage", type=int, default=2,
-                    help='the batch of per id')
-parser.add_argument("--batchtest", type=int, default=12,
-                    help='input batch size for test')
-parser.add_argument('--test_only', action='store_true',
-                    help='set this option to test the model')
-parser.add_argument('--sampler', type=str, default='True',
-                    help='do use sampler in dataloader')
+        self.n_ch = 2
+        self.chs = 512 // self.n_ch
+
+        osnet = osnet_x1_0(pretrained=True)
+
+        self.backone = nn.Sequential(
+            osnet.conv1,
+            osnet.maxpool,
+            osnet.conv2,
+            osnet.conv3[0]
+        )
+
+        conv3 = osnet.conv3[1:]
+
+        self.global_branch = nn.Sequential(copy.deepcopy(
+            conv3), copy.deepcopy(osnet.conv4), copy.deepcopy(osnet.conv5))
+
+        self.partial_branch = nn.Sequential(copy.deepcopy(
+            conv3), copy.deepcopy(osnet.conv4), copy.deepcopy(osnet.conv5))
+
+        self.channel_branch = nn.Sequential(copy.deepcopy(
+            conv3), copy.deepcopy(osnet.conv4), copy.deepcopy(osnet.conv5))
+
+        self.global_pooling = nn.AdaptiveAvgPool2d((1, 1))
+        self.partial_pooling = nn.AdaptiveAvgPool2d((2, 1))
+        self.channel_pooling = nn.AdaptiveAvgPool2d((1, 1))
+
+        reduction = BNNeck3(512, args.num_classes,
+                            args.feats, return_f=True)
+
+        self.reduction_0 = copy.deepcopy(reduction)
+        self.reduction_1 = copy.deepcopy(reduction)
+        self.reduction_2 = copy.deepcopy(reduction)
+        self.reduction_3 = copy.deepcopy(reduction)
+        self.reduction_4 = copy.deepcopy(reduction)
+
+        self.shared = nn.Sequential(nn.Conv2d(
+            self.chs, args.feats, 1, bias=False), nn.BatchNorm2d(args.feats), nn.ReLU(True))
+        self.weights_init_kaiming(self.shared)
+
+        self.reduction_ch_0 = BNNeck(
+            args.feats, args.num_classes, return_f=True)
+        self.reduction_ch_1 = BNNeck(
+            args.feats, args.num_classes, return_f=True)
+
+        # if args.drop_block:
+        #     print('Using batch random erasing block.')
+        #     self.batch_drop_block = BatchRandomErasing()
+        # print('Using batch drop block.')
+        # self.batch_drop_block = BatchDrop(
+        #     h_ratio=args.h_ratio, w_ratio=args.w_ratio)
+        self.batch_drop_block = BatchFeatureErase_Top(512, OSBlock)
+
+        self.activation_map = args.activation_map
+
+    def forward(self, x):
+        # if self.batch_drop_block is not None:
+        #     x = self.batch_drop_block(x)
+
+        x = self.backone(x)
+
+        glo = self.global_branch(x)
+        par = self.partial_branch(x)
+        cha = self.channel_branch(x)
+
+        if self.activation_map:
+            glo_ = glo
+
+        if self.batch_drop_block is not None:
+            glo_drop, glo = self.batch_drop_block(glo)
+
+        if self.activation_map:
+
+            _, _, h_par, _ = par.size()
+
+            fmap_p0 = par[:, :, :h_par // 2, :]
+            fmap_p1 = par[:, :, h_par // 2:, :]
+            fmap_c0 = cha[:, :self.chs, :, :]
+            fmap_c1 = cha[:, self.chs:, :, :]
+            print('Generating activation maps...')
+
+            return glo, glo_, fmap_c0, fmap_c1, fmap_p0, fmap_p1
+
+        glo_drop = self.global_pooling(glo_drop)
+        glo = self.channel_pooling(glo)  # shape:(batchsize, 512,1,1)
+        g_par = self.global_pooling(par)  # shape:(batchsize, 512,1,1)
+        p_par = self.partial_pooling(par)  # shape:(batchsize, 512,2,1)
+        cha = self.channel_pooling(cha)  # shape:(batchsize, 256,1,1)
+
+        p0 = p_par[:, :, 0:1, :]
+        p1 = p_par[:, :, 1:2, :]
+
+        f_glo = self.reduction_0(glo)
+        f_p0 = self.reduction_1(g_par)
+        f_p1 = self.reduction_2(p0)
+        f_p2 = self.reduction_3(p1)
+        f_glo_drop = self.reduction_4(glo_drop)
+
+        ################
+
+        c0 = cha[:, :self.chs, :, :]
+        c1 = cha[:, self.chs:, :, :]
+        c0 = self.shared(c0)
+        c1 = self.shared(c1)
+        f_c0 = self.reduction_ch_0(c0)
+        f_c1 = self.reduction_ch_1(c1)
+
+        ################
+
+        fea = [f_glo[-1], f_glo_drop[-1], f_p0[-1]]
+
+        if not self.training:
+
+            return torch.stack([f_glo[0], f_glo_drop[0], f_p0[0], f_p1[0], f_p2[0], f_c0[0], f_c1[0]], dim=2)
+            # return torch.stack([f_glo_drop[0], f_p0[0], f_p1[0], f_p2[0], f_c0[0], f_c1[0]], dim=2)
+
+        return [f_glo[1], f_glo_drop[1], f_p0[1], f_p1[1], f_p2[1], f_c0[1], f_c1[1]], fea
+
+    def weights_init_kaiming(self, m):
+        classname = m.__class__.__name__
+        if classname.find('Linear') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+            nn.init.constant_(m.bias, 0.0)
+        elif classname.find('Conv') != -1:
+            nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0.0)
+        elif classname.find('BatchNorm') != -1:
+            if m.affine:
+                nn.init.constant_(m.weight, 1.0)
+                nn.init.constant_(m.bias, 0.0)
 
 
-parser.add_argument('--model', default='LMBN_n_student_2_12', help='model name')
-parser.add_argument('--loss', type=str, default='0.5*CrossEntropy+0.5*MSLoss',
-                    help='loss function configuration')
-parser.add_argument("--if_labelsmooth", action='store_true',
-                    help='Label Smoothing Trick')
-parser.add_argument("--bnneck", action='store_true',
-                    help='Apply bnneck before classifier, refer to BoT paper')
-parser.add_argument("--feat_inference", type=str, default='after',
-                    help='Apply bnneck before classifier, refer to BoT paper')
-parser.add_argument("--drop_block", action='store_true',
-                    help='Apply batch drop block')
-parser.add_argument("--w_ratio", type=float, default=1.0,
-                    help='w_ratio of batch drop block')
-parser.add_argument("--h_ratio", type=float, default=0.3,
-                    help='w_ratio of batch drop block')
+if __name__ == '__main__':
+    # Here I left a simple forward function.
+    # Test the model, before you train it.
+    import argparse
 
+    parser = argparse.ArgumentParser(description='MGN')
+    parser.add_argument('--num_classes', type=int, default=751, help='')
+    parser.add_argument('--bnneck', type=bool, default=True)
+    parser.add_argument('--pool', type=str, default='max')
+    parser.add_argument('--feats', type=int, default=512)
+    parser.add_argument('--drop_block', type=bool, default=True)
+    parser.add_argument('--w_ratio', type=float, default=1.0, help='')
 
-parser.add_argument('--act', type=str, default='relu',
-                    help='activation function')
-parser.add_argument('--pool', type=str, default='avg', help='pool function')
-parser.add_argument('--feats', type=int, default=512,
-                    help='number of feature maps')
-parser.add_argument('--height', type=int, default=384,
-                    help='height of the input image')
-parser.add_argument('--width', type=int, default=128,
-                    help='width of the input image')
-parser.add_argument('--num_classes', type=int, default=751, help='')
-parser.add_argument('--T', type=int, default=1,
-                    help='number of iterations of computing group loss')
-parser.add_argument('--num_anchors', type=int, default=2,
-                    help='number of iterations of computing group loss')
+    args = parser.parse_args()
+    net = MCMP_n(args)
+    # net.classifier = nn.Sequential()
+    # print([p for p in net.parameters()])
+    # a=filter(lambda p: p.requires_grad, net.parameters())
+    # print(a)
 
-
-parser.add_argument("--lr", type=float, default=6e-4, help='learning rate')
-parser.add_argument('--optimizer', default='ADAM', choices=('SGD', 'ADAM',
-                                                            'NADAM', 'RMSprop'), help='optimizer to use (SGD | ADAM | NADAM | RMSprop)')
-parser.add_argument('--momentum', type=float, default=0.9, help='SGD momentum')
-parser.add_argument('--dampening', type=float, default=0, help='SGD dampening')
-parser.add_argument('--nesterov', action='store_true', help='SGD nesterov')
-parser.add_argument('--beta1', type=float, default=0.9, help='ADAM beta1')
-parser.add_argument('--beta2', type=float, default=0.999, help='ADAM beta2')
-parser.add_argument('--amsgrad', action='store_true', help='ADAM amsgrad')
-parser.add_argument('--epsilon', type=float, default=1e-8,
-                    help='ADAM epsilon for numerical stability')
-parser.add_argument('--gamma', type=float, default=0.1,
-                    help='learning rate decay factor for step decay')
-parser.add_argument('--weight_decay', type=float,
-                    default=5e-4, help='weight decay')
-parser.add_argument('--decay_type', type=str, default='step',
-                    help='learning rate decay type')
-parser.add_argument('--lr_decay', type=int, default=60,
-                    help='learning rate decay per N epochs')
-parser.add_argument('--warmup', type=str, default='constant', choices=['constant', 'linear'],
-                    help='warmup iteration, option: linear, constant')
-parser.add_argument('--pcb_different_lr', type=str,
-                    default='True', help='use different lr in pcb optimizer')
-parser.add_argument("--cosine_annealing", action='store_true',
-                    help='if raise, cosine_annealing')
-parser.add_argument("--w_cosine_annealing", action='store_true',
-                    help='if raise, warmup cosine_annealing')
-
-
-parser.add_argument('--parts', type=int, default=6, help='parts of PCB model')
-parser.add_argument("--margin", type=float, default=0.8, help='')
-parser.add_argument("--re_rank", action='store_true',
-                    help='if raise, use re-ranking')
-parser.add_argument("--cutout", action='store_true',
-                    help='if raise, use cutout augmentation')
-
-parser.add_argument("--random_erasing", action='store_true', help='')
-parser.add_argument("--probability", type=float, default=0.5, help='')
-
-parser.add_argument('--save', type=str, default='test',
-                    help='folder name to save')
-parser.add_argument('--load', type=str, default='', help='folder name to load')
-parser.add_argument('--pre_train', type=str, default='',
-                    help='pre-trained model path')
-parser.add_argument("--activation_map", action='store_true',
-                    help='if raise, return feature activation map')
-# For Neptune
-parser.add_argument('--nep_token', '-n', type=str,
-                    default='', help='neptune_api_token')
-parser.add_argument('--nep_id', type=str,
-                    default='', help='neptune_experiment_id')
-parser.add_argument('--nep_name', type=str,
-                    default='x.ji/mcmp', help='neptune_project_name')
-
-parser.add_argument('--reset', action='store_true', help='reset the training')
-# parser.add_argument("--savedir", type=str, default='saved_models', help='directory name to save')
-# parser.add_argument("--outdir", type=str, default='out', help='')
-# parser.add_argument("--resume", action='store_true', help='whether resume training from specific checkpoint')
-# parser.add_argument('--save_models', action='store_true', help='save all intermediate models')
-
-# for wandb
-parser.add_argument('--wandb', action='store_true', help='use wandb')
-parser.add_argument('--wandb_name', type=str, default='', help='wandb project name')
-
-parser.add_argument(
-    '--seed',
-    type=int,
-    default=251,
-    help='random seed for reproducibility (default: 42)'
-)
-
-
-args = parser.parse_args()
-
-for arg in vars(args):
-     if vars(args)[arg] == 'True':
-          vars(args)[arg] = True
-     elif vars(args)[arg] == 'False':
-          vars(args)[arg] = False
+    print(net)
+    input = Variable(torch.FloatTensor(8, 3, 384, 128))
+    net.eval()
+    output = net(input)
+    print(output.shape)
+    print('net output size:')
+    # print(len(output))
+    # for k in output[0]:
+    #     print(k.shape)
+    # for k in output[1]:
+    #     print(k.shape)
