a04d627785eca5ceb02d38ef5f0b3c44f1b6014d
coffeeboy123 <ih369369@naver.com>
2025-07-01 13:16:29 +0900

    a

--- utils/utility.py
+++ utils/utility.py
@@ -34,9 +34,9 @@ class checkpoint():
         if args.load == '':
             if args.save == '':
                 args.save = now
-            self.dir = '/content/gdrive/MyDrive/LightMBN_SAVE_transformers' + '/experiment/' + args.save
+            self.dir = '/content/gdrive/MyDrive/LightMBN_REAL_SAVE' + '/experiment/' + args.save
         else:
-            self.dir = '/content/gdrive/MyDrive/LightMBN_SAVE_transformers' + '/experiment/' + args.save
+            self.dir = '/content/gdrive/MyDrive/LightMBN_REAL_SAVE' + '/experiment/' + args.save
             if not os.path.exists(self.dir):
                 args.load = ''
             args.save = args.load
