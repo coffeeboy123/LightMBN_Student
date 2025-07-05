6c2e578f98c5a6a617c4dc5804f5f39d795cebcf
coffeeboy123 <ih369369@naver.com>
2025-07-05 20:58:31 +0900

    a

--- utils/utility.py
+++ utils/utility.py
@@ -40,9 +40,9 @@ class checkpoint():
         if args.load == '':
             if args.save == '':
                 args.save = now
-            self.dir = '/content/gdrive/MyDrive/SAVE' + '/experiment/' + args.save
+            self.dir = '/content/gdrive/MyDrive/SAVE_again' + '/experiment/' + args.save
         else:
-            self.dir = '/content/gdrive/MyDrive/SAVE' + '/experiment/' + args.save
+            self.dir = '/content/gdrive/MyDrive/SAVE_again' + '/experiment/' + args.save
             if not os.path.exists(self.dir):
                 args.load = ''
             args.save = args.load
